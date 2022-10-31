cd4ec573932813e04357862d5b608f7c99293d66
Travis Brown <85965795+tychoterra@users.noreply.github.com>
2022-10-31 15:55:53 -0400

    created a new post request within the sumbit listener - refactored the code to make it more DRY - created renderPost function to render the new posts to the top of the blog-list

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-fetch("https://apis.scrimba.com/jsonplaceholder/posts")
-  .then((res) => res.json())
-  .then((data) => {
-    const postArr = data.slice(0, 5);
-    let html = "";
-    for (let post of postArr) {
-      html += `
+let postsArray = [];
+
+function renderPosts() {
+  let html = "";
+  for (let post of postsArray) {
+    html += `
         <h2>${post.title}</h2>
         <p>${post.body}</p>
         <hr />
       `;
-      document.querySelector(".posts").innerHTML = html;
-    }
+    document.querySelector(".posts").innerHTML = html;
+  }
+}
+
+fetch("https://apis.scrimba.com/jsonplaceholder/posts")
+  .then((res) => res.json())
+  .then((data) => {
+    postsArray = data.slice(0, 5);
+    renderPosts();
   });
 
 const postForm = document.querySelector(".post-form");
@@ -22,5 +28,14 @@ postForm.addEventListener("submit", (e) => {
     title: titleInput,
     body: bodyInput,
   };
-  console.log(post);
+  fetch("https://apis.scrimba.com/jsonplaceholder/posts", {
+    method: "POST",
+    body: JSON.stringify(post),
+    headers: { "Content-Type": "application/json" },
+  })
+    .then((res) => res.json())
+    .then((post) => {
+      postsArray.unshift(post);
+      renderPosts();
+    });
 });
